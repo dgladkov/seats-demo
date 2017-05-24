3d4a327b4667176eeb61d1b2b6f9a233bcbf12d0
Dmitry Gladkov <dmitry.gladkov@gmail.com>
2017-05-24 18:06:10 +0300

    use konva without react bindings

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react';
+import {
+  Group,
+  Layer,
+  Rect,
+  Stage,
+  Text,
+} from 'konva';
 import { connect } from 'react-redux';
-import {Layer, Rect, Stage, Group, Text} from 'react-konva';
 import { toggleSeatAvailability } from './actions/seats';
 
 const SEAT_SIZE = 50;
@@ -9,59 +15,73 @@ const SEATS_PER_ROW = 100;
 const NUMBER_FONT_SIZE = 12;
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.drawGroup = this.drawGroup.bind(this);
+  }
+
   componentDidMount() {
-    this.refs.preview.src = this.refs.svg.toDataURL();
+    const stage = new Stage({
+      width: (SEAT_SIZE + SEAT_SPACING) * SEATS_PER_ROW + SEAT_SPACING,
+      height: Math.ceil(this.props.seats.length / SEATS_PER_ROW) * (SEAT_SIZE + SEAT_SPACING) + SEAT_SPACING,
+      container: this.refs.container,
+    });
+    this.layer = new Layer();
+    this.layer.on('click', 'Group', (event) => {
+      this.props.toggleSeatAvailability(event.currentTarget.index);
+    });
+    this.props.seats.forEach(this.drawGroup);
+    stage.add(this.layer);
+    this.refs.preview.src = this.layer.toDataURL();
   }
-  componentDidUpdate() {
-    this.refs.preview.src = this.refs.svg.toDataURL();
+
+  componentWillReceiveProps(nextProps) {
+    let changed = false;
+    this.props.seats.forEach((seat, index) => {
+      const nextSeat = nextProps.seats[index];
+      if (seat.available !== nextSeat.available) {
+        this.layer.children[index].children[0].fill(nextSeat.available ? '#00FF00' : '#FF0000');
+        this.layer.children[index].draw();
+        changed = true;
+      }
+    });
+    if (changed) {
+      this.refs.preview.src = this.layer.toDataURL();
+    }
   }
+
+  drawGroup(seat, index) {
+    const col = index % SEATS_PER_ROW;
+    const row = Math.floor(index / SEATS_PER_ROW);
+    const groupX = (SEAT_SIZE + SEAT_SPACING) * col + SEAT_SPACING;
+    const groupY = row * (SEAT_SIZE + SEAT_SPACING) + SEAT_SPACING;
+
+    const group = new Group({
+      x: groupX,
+      y: groupY,
+    });
+    const rect = new Rect({
+      width: SEAT_SIZE,
+      height: SEAT_SIZE,
+      fill: seat.available ? '#00FF00' : '#FF0000',
+    });
+    const text = new Text({
+      text: `${row + 1}:${col + 1}`,
+      align: 'center',
+      width: SEAT_SIZE,
+      y: (SEAT_SIZE / 2) - (NUMBER_FONT_SIZE / 2),
+    });
+    group.add(rect);
+    group.add(text);
+    this.layer.add(group);
+  }
+
   render() {
     return (
       <div>
-        <Stage
-          width={(SEAT_SIZE + SEAT_SPACING) * SEATS_PER_ROW + SEAT_SPACING}
-          height={Math.ceil(this.props.seats.length / SEATS_PER_ROW) * (SEAT_SIZE + SEAT_SPACING) + SEAT_SPACING}>
-          <Layer ref="svg">
-            {this.props.seats.map((seat, index) => {
-              const row = Math.floor(index / SEATS_PER_ROW);
-              const col = index % SEATS_PER_ROW;
-              return (
-                <Group
-                  key={index}
-                  x={(SEAT_SIZE + SEAT_SPACING) * col + SEAT_SPACING}
-                  y={row * (SEAT_SIZE + SEAT_SPACING) + SEAT_SPACING}
-                  onClick={() => this.props.toggleSeatAvailability(index)}
-                >
-                  <Rect
-                    width={SEAT_SIZE}
-                    height={SEAT_SIZE}
-                    fill={seat.available ? "#00FF00" : "#FF0000"}
-                    onClick={this.handleClick}
-                  />
-                  <Text
-                    text={`${row + 1}:${col + 1}`}
-                    align="center"
-                    width={SEAT_SIZE}
-                    y={(SEAT_SIZE / 2) - (NUMBER_FONT_SIZE / 2)}
-                  />
-                </Group>
-              );
-            })}
-          </Layer>
-        </Stage>
-        <div style={{position: 'fixed', bottom: 0, left: 0}}>
-          <img ref="preview" style={{ width: '100%'}} />
-          <div style={{
-            position: 'absolute',
-            top: 0,
-            left: 0,
-            height: '100px',
-            width: '100px',
-            borderWidth: '1px',
-            borderColor: '#000000',
-            zIndex: 10000,
-          }} />
-        </div>
+        <div ref="container" />
+        <img ref="preview" style={ {width: '100%' }} />
       </div>
     );
   }
